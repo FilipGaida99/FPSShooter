7f39b30dc04863b6a33abc87be4dfc12dee22256
Donkrzawayan <43960303+Donkrzawayan@users.noreply.github.com>
2021-04-20 19:09:35 +0200

    LoadingScreen and concept of Manager of scenes v2

---
+++ FPSShooter/Assets/UI/ScenesManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesManager
+{
+    private static ScenesManager instance;
+
+    public static ScenesManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new ScenesManager();
+
+            return instance;
+        }
+    }
+
+    public enum Scene { Menu, LoadingScreen, FirstLevel }
+
+    private Action onSManagerCallback;
+
+    public void Load(Scene scene)
+    {
+        // Set the SManager callback action to load the target scene
+        onSManagerCallback = () =>
+        {
+            SceneManager.LoadScene(scene.ToString());
+        };
+
+        SceneManager.LoadScene(Scene.LoadingScreen.ToString());
+    }
+
+    public void SManagerCallback()
+    {
+        if(onSManagerCallback != null)
+        {
+            onSManagerCallback();
+            onSManagerCallback = null;
+        }
+    }
+}
